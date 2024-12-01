e6bbe6d86941a1a9a6266d4514a6ea3eefe16c5c
irfan2622 <137972532+irfan2622@users.noreply.github.com>
2024-12-01 19:38:44 +0700

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -23,11 +23,16 @@ DRIVE_FOLDER_URL = "https://drive.google.com/drive/folders/1PbTbPboHnqs-eCr63gzY
 
 # Fungsi untuk membuat layanan Google Drive
 def create_drive_service():
+    # Membaca kredensial dari file JSON
+    with open(GOOGLE_CREDENTIALS_PATH, 'r') as json_file:
+        credentials_info = json.load(json_file)
+    
     credentials = service_account.Credentials.from_service_account_info(
-        json.loads(GOOGLE_CREDENTIALS_JSON), scopes=["https://www.googleapis.com/auth/drive"]
+        credentials_info, scopes=["https://www.googleapis.com/auth/drive"]
     )
     service = build("drive", "v3", credentials=credentials)
     return service
+
 
 # Fungsi untuk mengambil ID folder dari URL
 def extract_folder_id(folder_url):
